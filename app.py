a982c253546e7394861b41492cb2d3839c5721c6
Stephanie <80798182+sphetanie@users.noreply.github.com>
2021-12-16 08:16:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ x = int(input("Digite um número inteiro: "))
 
 for i in range(4):
     y = int(input("Digite um número inteiro: "))
-    if y > x:
+    if (y > x):
         x = y
 print(f"O maior número é {x}")
